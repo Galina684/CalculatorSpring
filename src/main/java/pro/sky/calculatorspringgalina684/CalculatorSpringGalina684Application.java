589e80857380f85781ed308c977ca3559498c57f
Galina684 <galina684@gmail.com>
2023-09-05 00:46:15 +0300

    Hw4

---
+++ src/main/java/pro/sky/calculatorspringgalina684/CalculatorSpringGalina684Application.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorspringgalina684;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSpringGalina684Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSpringGalina684Application.class, args);
+	}
+
+}
